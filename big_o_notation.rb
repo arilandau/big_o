79dc75b15f4692b67f6ef47bbec765ddbfd6b5ef
Arielle Landau <arielle.landau@launchacademy.com>
2017-09-18 14:06:12 -0400

    adds binary search O(log n) time

--- big_o_notation.rb
+++ big_o_notation.rb
@@ -3,35 +3,42 @@
 def is_first_number_nil(array)
   if array[0].nil?
     true
+    # return "I went around once!"
   else
     false
+    # return "I went around once!"
   end
 end
+# puts is_first_number_nil([nil,2,3,4,5])
+# puts is_first_number_nil([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+# puts is_first_number_nil([nil,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40])
 
-puts is_first_number_nil([nil])
-puts is_first_number_nil([1,2,3,4,5,6,7,8,9,10])
 
 # o(n) Linear Time
 def is_any_number_nil(array)
-  array.each do |x|
+  array.each_with_index do |x, index|
     if x.nil?
+      puts "I went around #{index + 1} times!"
       return true
-      break
     end
+    puts "I went around #{index + 1} times!"
   end
 
   return false
 end
 
-puts is_first_number_nil([nil])
-puts is_first_number_nil([1,2,3,4,nil,6,7,8,9,nil])
+# puts is_any_number_nil([1,2,3,4,nil])
+# puts is_any_number_nil([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,nil])
+# puts is_any_number_nil([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,nil])
 
 # o(n ** 2) Quadratic Time
 def contains_duplicates(array)
   unique_numbers = []
   found = false
-  array.each do |x|
-    unique_numbers.each do |y|
+  array.each_with_index do |x, outer_index|
+    puts "The outer circle went around #{outer_index + 1} times!"
+    unique_numbers.each_with_index do |y, inner_index|
+      puts "The inner circle went around #{inner_index + 1} times!"
       if x == y
         found = true
         break
@@ -41,26 +48,55 @@ def contains_duplicates(array)
       unique_numbers << x
     end
   end
-  printf "exists: input_size: %6d, inner_loops: %d\n", array.length, counter
   return found
 end
-
-puts contains_duplicates([1,2,3,4,5,6,7,8,9,10])
-puts contains_duplicates([1,2,3,4,5,6,7,8,10,10])
-
-
+# puts contains_duplicates([1,1])
+# puts contains_duplicates([1,2,2])
+# puts contains_duplicates([1,2,3,4,4])
+# puts contains_duplicates([1,2,3,4,5,6,6])
+# puts contains_duplicates([1,2,3,4,5,6,7,7])
+# puts contains_duplicates([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,39])
+#
+#
 # o(2 ** n) Exponential Time
 
 def fibonacci(n)
   return n if n == 0 || n == 1
   if n >= 2
+    puts "I went around again!"
     return (fibonacci(n-2) + fibonacci(n-1))
   end
 end
 
-puts fibonacci(2)
-puts fibonacci(3)
-puts fibonacci(5)
-puts fibonacci(8)
-puts fibonacci(13)
-puts fibonacci(1000)
+# puts fibonacci(2)
+# puts fibonacci(5)
+# puts fibonacci(20)
+# puts fibonacci(40)
+
+# o(log n) Logarithmic Time
+def binary_search(array, value)
+  first = 0
+  last = array.length - 1
+  i = 0
+
+  while first <= last
+    puts "I went around #{i} times!"
+    mid = (first + last) / 2
+
+    if array[mid] == value
+      return "#{value} found at position #{mid + 1}."
+    elsif array[mid] > value
+      last = mid - 1
+    elsif array[mid] < value
+      first = mid + 1
+    else
+      return "#{value} not found in this array."
+    end
+
+    i += 1
+  end
+end
+
+# puts binary_search([1,2,3,4,5], 5)
+# puts binary_search([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 20)
+# puts binary_search([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40], 40)
